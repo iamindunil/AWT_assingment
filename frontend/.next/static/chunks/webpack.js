07f8c0d194cd8c55788c3a6ffb4b3e751f5489e6
Indunil Kavinda <indunilkavinda1121@gmail.com>
2025-05-03 20:38:35 +0530

    updating

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03973fe65a1a3fa5"; }
+/******/ 		__webpack_require__.h = function() { return "71b44a775fa4871c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
